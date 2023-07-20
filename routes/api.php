a84b7cb767bea08b6e653713f00f5ed1fd96d69b
workremotely <payel333@gmail.com>
2023-07-20 18:04:15 +0530

    Created a controller and a route to use PATCH HTTP update method

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\API\V1\CompleteTaskController;
 use App\Http\Controllers\API\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
     Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
