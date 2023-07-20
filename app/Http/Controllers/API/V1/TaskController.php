10d51359a14beddf71176095dea8767e93677659
workremotely <payel333@gmail.com>
2023-07-20 15:38:21 +0530

    Added validation and implemented store method

--- app/Http/Controllers/API/V1/TaskController.php
+++ app/Http/Controllers/API/V1/TaskController.php
@@ -20,20 +20,14 @@ class TaskController extends Controller
         return TaskResource::collection(Task::all());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTaskRequest $request)
     {
-        //
+        $task = Task::create($request->validated());
+        return TaskResource::make($task);
     }
 
     /**
--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -11,7 +11,7 @@ class StoreTaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class StoreTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'max:255|required|string'
         ];
     }
 }
