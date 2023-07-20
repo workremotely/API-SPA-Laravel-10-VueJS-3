87f0b527c0da178162fa39f0c73736693aadb032
workremotely <payel333@gmail.com>
2023-07-20 20:30:36 +0530

    Activated destroy function in main controller

--- app/Http/Controllers/API/V1/TaskController.php
+++ app/Http/Controllers/API/V1/TaskController.php
@@ -52,6 +52,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
